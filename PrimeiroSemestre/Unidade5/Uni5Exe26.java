cf4453d8f5cda4016a1eb0255a971034c20acfe3
rafaelsiqueirapimentel02 <rafaelsiqueirapimentel02@gmail.com>
2026-05-11 10:44:20 -0300

    atividade 26, e começo da 27

--- PrimeiroSemestre/Unidade5/Uni5Exe26.java
+++ PrimeiroSemestre/Unidade5/Uni5Exe26.java
@@ -6,6 +6,33 @@ public class Uni5Exe26 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        
+        System.out.print("A partir de qual valor Astolfo se recusa a pagar: ");
+        double negacao = scanner.nextDouble();
+
+        double pedagio = 0; 
+        int distancia = 0;
+        int recusa = 0;
+        int trecho150km = 0;
+        int trechos = 0;
+         
+        while (pedagio >= 0){
+            trechos++;
+            System.out.print("O valor do pedágio e a distância(KM): ");
+            pedagio = scanner.nextDouble();
+            distancia = scanner.nextInt();
+            
+            if (pedagio >= negacao) {
+                recusa++;
+            } 
+            if (pedagio < negacao && distancia > 150) {
+                trecho150km++;
+            }
+        }
+
+        System.out.print("Trechos que se recusou a pagar " + recusa + "\n");
+        System.out.print("Trechos informados " + (trechos - 1) + "\n");
+        System.out.print("Trechos acima de 150km que ele aceita pagar " + trecho150km);
+
+        scanner.close();
     }
 }
--- PrimeiroSemestre/Unidade5/Uni5Exe27.java
+++ PrimeiroSemestre/Unidade5/Uni5Exe27.java
@@ -1,9 +1,59 @@
-import java.util.Scanner;
-
 package PrimeiroSemestre.Unidade5;
+
+import java.util.Scanner;
 
 public class Uni5Exe27 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        int escolha = 1;
+        int pecasTotalManha = 0;
+        int pecasTotalTarde = 0;
+        double salário = 0;
+
+        while (escolha == 1) {
+            System.out.print("Digite o dia de Abril: ");
+            int dia = scanner.nextInt();
+
+            while (dia < 1 || dia > 30) {
+                System.out.print("Dia inválido");
+                dia = scanner.nextInt();
+            }
+
+            if (dia >= 1 || dia <= 15) {
+                System.out.print("Total de peças produzidas pela manhã: ");
+                int manha = scanner.nextInt();
+                pecasTotalManha += manha;
+
+                System.out.print("Total de peças produzidas pela tarde: ");
+                int tarde = scanner.nextInt();
+                pecasTotalTarde += tarde;
+
+
+                if (pecasTotalManha >= 30 && pecasTotalTarde >=30) {
+                    int pecasTotal = pecasTotalManha + pecasTotalTarde;
+                    salário = 0.80 * pecasTotal;
+
+                } else {
+                    int pecasTotal = pecasTotalManha + pecasTotalTarde;
+                    salário = 0.50 * pecasTotal;
+                }
+            }
+
+            else if (dia >= 16 && dia <= 30) {
+                System.out.print("Total de peças produzidas pela manhã: ");
+                int manha = scanner.nextInt();
+                pecasTotalManha += manha;
+
+                System.out.print("Total de peças produzidas pela tarde: ");
+                int tarde = scanner.nextInt();
+                pecasTotalTarde += tarde;
+             
+                double manha2 = 0.40 * pecasTotalManha;
+                double tarde2 = 0.30 * pecasTotalTarde; 
+             }
+        }
+
+
     }
 }
